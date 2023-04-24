dcb1600a616e2df7fe71ab1cd9c7efdc4b13cd3e
muloh <rapyc@live.com>
2023-04-23 20:30:05 -0400

    add currency select tag

--- src/components/AllocationForm.js
+++ src/components/AllocationForm.js
@@ -58,7 +58,7 @@ const AllocationForm = (props) => {
                         <option defaultValue value="Add" name="Add">Add</option>
                 <option value="Reduce" name="Reduce">Reduce</option>
                   </select>
-
+                    <span>£</span>
                     <input
                         required='required'
                         type='number'
@@ -71,6 +71,14 @@ const AllocationForm = (props) => {
                     <button className="btn btn-primary" onClick={submitEvent} style={{ marginLeft: '2rem' }}>
                         Save
                     </button>
+
+                    <select className="custom-select" id="inputGroupSelect01" onChange={(event) => setName(event.target.value)}>
+                        <option defaultValue>Currency(£ Pound)</option>
+                        <option value="$Dollar" name="Dollar"> $ Dollar</option>
+                        <option value="£Pound" name="Pound">£ Pound</option>
+                        <option value="€Euro" name="Euro">€ Euro</option>
+                        <option value="₹Ruppee" name="Ruppee">₹ Ruppee</option>
+                    </select>
                 </div>
                 </div>
 
